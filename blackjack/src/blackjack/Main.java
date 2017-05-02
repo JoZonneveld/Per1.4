7a145b2bf3989fbc9fd19440edb7dc17bd0414f4
JoZonneveld <joostz14@gmail.com>
2017-05-02 17:34:18 +0200

    .

--- blackjack/src/blackjack/Main.java
+++ blackjack/src/blackjack/Main.java
@@ -9,11 +9,17 @@ public class Main {
         boolean game = true;
         Game start = new Game("Joost");
 
-        //System.out.println("Welkom " + start.player.Name);
-        //System.out.println("Let us play a game ");
+        System.out.println("Welkom " + start.player.Name);
+        System.out.println("Let us play a game ");
 
-        //System.out.println(start.Deck);
-        System.out.println(start.player.Hand);
+        System.out.println(start.Deck.get(0).number);
+
+        start.Deck.remove(0);
+
+        System.out.println(start.Deck.get(0).number);
+
+        start.Deck.remove(0);
+        //System.out.println(start.player.Hand);
 
         //start.addCard();
     }
