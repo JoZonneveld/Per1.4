d1fc155b559a3026b8c490fb61de61ef18c58f7c
JoZonneveld <joostz14@gmail.com>
2017-05-19 10:29:33 +0200

    .

---
+++ Class Practice/LinkedList3/LinkedList3/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList3
+{
+    public interface ILinkedList<T>
+    {
+        void Visit(Action<LinkedListNode<T>> onNode, Action onEmpty);
+        U Visit<U>(Func<LinkedListNode<T>> onNode, Func<T> onEmpty);
+    }
+
+    public class Empty<T> : ILinkedList<T>
+    {
+        public void Visit(Action<LinkedListNode<T>> onNode, Action onEmpty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public U Visit<U>(Func<LinkedListNode<T>> onNode, Func<T> onEmpty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Node<T> : ILinkedList<T>
+    {
+        private T Value;
+        private ILinkedList<T> Tail;
+        public Node(T value, ILinkedList<T> tail)
+        {
+            this.Value = value;
+            this.Tail = tail;
+        }
+
+        public void Visit(Action<LinkedListNode<T>> onNode, Action onEmpty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public U Visit<U>(Func<LinkedListNode<T>> onNode, Func<T> onEmpty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Class Practice/LinkedList3/LinkedList3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILinkedList<int> Collection1 = new Node<int>(1, new Node<int>(2, new Node<int>(3, new Empty<int>())));
+            PrintCollection(Collection1);
+        }
+
+        static void PrintCollection<T>(LinkedList<T> collection)
+        {
+            collection.Visit(node =>
+            {
+                Console.WriteLine(node.Value);
+                PrintCollection<T>(node.Tail);
+            },
+                () =>
+                {
+                    Console.WriteLine("Im Empty");
+                }
+            );
+        }
+    }
+}
